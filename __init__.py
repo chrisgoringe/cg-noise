936e4cc4b3d66626b11db2ff1e785f90d5b10ddf
Chris <chris.goringe@gmail.com>
2023-09-12 09:21:08 +1000

    stupid

--- __init__.py
+++ __init__.py
@@ -7,7 +7,9 @@ except:
     repo_path = os.path.join(os.path.dirname(folder_paths.__file__), 'custom_nodes', 'cg_custom_core')  
     repo = git.Repo.clone_from('https://github.com/chrisgoringe/cg-custom-core.git/', repo_path)
     repo.git.clear_cache()
-    repo.close()sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
+    repo.close()
+    
+sys.path.insert(0,os.path.dirname(os.path.realpath(__file__)))
     
 from .noise import *
 
