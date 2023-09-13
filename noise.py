f1e651dab3059dec486e305446716333cc6e29d6
Chris <chris.goringe@gmail.com>
2023-09-14 09:46:45 +1000

    KSampler and KSamplerAdvanced

--- noise.py
+++ noise.py
@@ -1,33 +1,45 @@
-from custom_nodes.cg_custom_core.base import BaseNode
 import comfy.sample
+from nodes import KSampler, KSamplerAdvanced
+
+def get_mixed_noise_function(original_noise_function, variation_seed, variation_weight):
+    def prepare_mixed_noise(latent_image, seed, batch_inds):
+        different_noise = original_noise_function(latent_image, variation_seed, batch_inds)
+        original_noise = original_noise_function(latent_image, seed, batch_inds)
+        return original_noise * (1.0-variation_weight) + different_noise * (variation_weight) 
+    return prepare_mixed_noise
+
+def insert_variation_inputs(input_types):
+    required = {}
+    for rkey in input_types['required']:
+        required[rkey] = input_types['required'][rkey]
+        if rkey=="seed" or rkey=="noise_seed":
+            required['variation_seed'] = ("INT", {"min": 0, "max": 0xffffffffffffffff})
+            required['variation_weight'] = ("FLOAT", {"default": 0.001, "min": 0, "max": 1, "step": 0.001})
+    input_types['required'] = required
+    return input_types    
+
+class Variations():
+    CATEGORY = "sampling"
+    FUNCTION = "func"
+    clazz = None
+    @classmethod
+    def INPUT_TYPES(cls):
+        return insert_variation_inputs(cls.clazz.INPUT_TYPES())
     
-class Hijack(BaseNode):
-    CATEGORY = "noise/hijack"
-    REQUIRED = {"latent": ("LATENT", {}), 
-                "variation":("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}), 
-                "weight": ("FLOAT", {"default": 0.001, "min": 0, "max": 1, "step": 0.001}) }
-    OPTIONAL = {"trigger": ("*",{})}
-    RETURN_TYPES = ("LATENT",)
-    RETURN_NAMES = ("latent",)
-    _original_noise_function = None
-    def func(self, latent, variation, weight, trigger=None):
-        if Hijack._original_noise_function==None:
-            Hijack._original_noise_function = comfy.sample.prepare_noise
-            def prepare_mixed_noise(latent_image, seed, batch_inds):
-                # generate the original noise second, so that the random number generator (which gets seeded) is left in the same state as if we didn't hijack
-                different_noise = Hijack._original_noise_function(latent_image, variation, batch_inds)
-                original_noise = Hijack._original_noise_function(latent_image, seed, batch_inds)
-                return original_noise * (1.0-weight) + different_noise * (weight) 
-            comfy.sample.prepare_noise = prepare_mixed_noise
-        return (latent,)
+    def __init__(self):
+        self.original_function_name = self.clazz.FUNCTION
+
+    def func(self, **kwargs):
+        variation_seed = kwargs.pop('variation_seed')
+        variation_weight = kwargs.pop('variation_weight')
+        original_noise_function = comfy.sample.prepare_noise
+        comfy.sample.prepare_noise = get_mixed_noise_function(original_noise_function, variation_seed, variation_weight)
+        results = getattr(self,self.clazz.FUNCTION)(**kwargs)
+        comfy.sample.prepare_noise = original_noise_function
+        return results    
     
-class UnHijack(BaseNode):
-    CATEGORY = "noise/hijack"
-    REQUIRED = {"latent": ("LATENT", {}) }
-    RETURN_TYPES = ("LATENT",)
-    RETURN_NAMES = ("latent",)
-    def func(self,latent):
-        if Hijack._original_noise_function:
-            comfy.sample.prepare_noise = Hijack._original_noise_function
-            Hijack._original_noise_function = None
-        return (latent,)
+class KSamplerVariations(Variations, KSampler):
+    clazz = KSampler
+    
+class KSamplerAdvancedVariations(Variations, KSamplerAdvanced):
+    clazz = KSamplerAdvanced
