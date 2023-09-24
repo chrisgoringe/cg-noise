109dc6d1acb7670f7a0595d32bdc1fd138dda304
Chris <chris.goringe@gmail.com>
2023-09-25 00:28:11 +1000

    add cotnrol_after_generate

--- js/seed_fixed.js
+++ js/seed_fixed.js
@@ -1,12 +1,19 @@
 import { app } from "../../../scripts/app.js";
+import { addValueControlWidget } from "../../../scripts/widgets.js";
 
 app.registerExtension({
 	name: "cg.noise.seedFixed",
 	version: 1,
 	async nodeCreated(node) {
-        if (node.widgets?.find((w) => w.name === 'variation_seed')) {
-            const cag = node.widgets.find((w) => w.name === 'control_after_generate');
-            cag.value = 'fixed'
+        const variationSeedWidgetIndex = node.widgets?.findIndex((w) => w.name === 'variation_seed');
+        if (variationSeedWidgetIndex > -1) {
+            const variationSeedWidget = node.widgets[variationSeedWidgetIndex];
+            const variationSeedValueControl = addValueControlWidget(node, variationSeedWidget, "fixed");
+            //variationSeedWidget.widget.linkedWidgets = [variationSeedValueControl];
+
+            node.widgets.splice(variationSeedWidgetIndex+1,0,node.widgets.pop());
+
+            node.widgets.forEach((w) => { if (w.name === 'control_after_generate') { w.value="fixed"}; });
         }
     }
 });
