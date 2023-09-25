a31edbd079af98d66b3a6c8e8d0b058a6aee32b9
Chris <chris.goringe@gmail.com>
2023-09-25 11:08:07 +1000

    set title color

--- hijack.py
+++ hijack.py
@@ -1,6 +1,9 @@
 from .noise import get_mixed_noise_function
+from custom_nodes.cg_custom_core.ui_decorator import ui_signal
+
 import comfy.sample
 
+@ui_signal('set_title_color')
 class Hijack():
     CATEGORY = "noise/hijack"
     FUNCTION = "func"
@@ -11,20 +14,24 @@ class Hijack():
                 "latent": ("LATENT", {}), 
                 "variation_seed":("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}), 
                 "variation_weight": ("FLOAT", {"default": 0.001, "min": 0, "max": 1, "step": 0.001}) 
-            }
+            },
+            "hidden" : { "id": "UNIQUE_ID" }
         }
     RETURN_TYPES = ("LATENT",)
     RETURN_NAMES = ("latent",)
     _original_noise_function = None
-    @classmethod
-    def func(cls, latent, variation_seed, variation_weight, trigger=None):
-        if cls._original_noise_function==None:
-            cls._original_noise_function = comfy.sample.prepare_noise
+    _hijack_node_id = None
+
+    def func(self, latent, variation_seed, variation_weight, id):
+        if self._original_noise_function==None:
+            self._original_noise_function = comfy.sample.prepare_noise
         else:
             print("In noise hijack, the noise function had already been hijacked - continuing, but this might be an error. Maybe you forgot to unhijack?")
-        comfy.sample.prepare_noise = get_mixed_noise_function(cls._original_noise_function, variation_seed, variation_weight)
-        return (latent,)
-    
+        comfy.sample.prepare_noise = get_mixed_noise_function(self._original_noise_function, variation_seed, variation_weight)
+        Hijack._hijack_node_id = id
+        return (latent, (f"id={Hijack._hijack_node_id}", "red"))
+
+@ui_signal('set_title_color')  
 class UnHijack():
     CATEGORY = "noise/hijack"
     FUNCTION = "func"
@@ -38,12 +45,11 @@ class UnHijack():
     RETURN_TYPES = ("LATENT",)
     RETURN_NAMES = ("latent",)
 
-    @classmethod
-    def func(cls,latent):
+    def func(slef,latent):
         if Hijack._original_noise_function:
             comfy.sample.prepare_noise = Hijack._original_noise_function
             Hijack._original_noise_function = None
-        return (latent,)
+        return (latent,(f"id={Hijack._hijack_node_id}","reset"))
     
 NODE_CLASS_MAPPINGS = {
     "Hijack" : Hijack, 
